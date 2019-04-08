b7cad0ec1bfc503cf85bf61f0bf9641bb15e564f
ahnoldmoon <munnt72@hotmail.com>
2019-04-08 21:24:34 +0900

    last the end : Complete

---
+++ src/org/queries.js
@@ -0,0 +1,15 @@
+import gql from "graphql-tag";
+
+export const HOME_PAGE = gql`
+    {
+        movies(limit:50, rating:7){
+            id
+            title
+            genres
+            rating
+            medium_cover_image
+        }
+    }
+`;
+
+// export const MOVIE_DEFAULTS = gql``;
